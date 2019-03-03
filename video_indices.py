8dd02c109819aab12305a7f213453026bcadf8dc
eashman123 <eashaank@gmail.com>
2019-03-03 10:14:59 -0800

    Fixed dl_youtube

--- video_indices.py
+++ video_indices.py
@@ -10,34 +10,39 @@ tolerance is the difference (in seconds) between two sequential timestamp tuples
 
 Output: List of timestamp tuples (start, end) for the summarized video
 """
-def video_indices(subtitle, text_summary, tolerance=0):
+# def video_indices(subtitle, text_summary, tolerance=0):
+#     text_summary = open(text_summary, "r").read()
+#     sents = [word_tokenize(s.lower()) for s in sent_tokenize(text_summary)]
+#     captions = webvtt.read(subtitle)
+#     coptions = webvtt.read(subtitle)
+#     cuttimes = []
+#     for sent in sents[-5:]:
+#         beg = ' '.join(sent[:3])
+#         last = ' '.join(sent[-4:-1])
+#         for i, caption in enumerate(captions):
+#             captext = caption.text.replace("\n", " ")
+#             captext = filter_captext(captext)
+#             if beg in captext:
+#                 start = caption.start
+#                 break
+#
+#         for coption in coptions:
+#             captext = coption.text.replace("\n", " ")
+#             captext = filter_captext(captext)
+#             if last in captext:
+#                 s, e = time_in_s([start, coption.end])
+#                 if e-s>0:
+#                     end = coption.end
+#                     break
+#         cuttimes.append([start, end])
+#         break
+#
+#     return cuttimes
+
+def video_indices(subtitle, text_summary):
     text_summary = open(text_summary, "r").read()
     sents = [word_tokenize(s.lower()) for s in sent_tokenize(text_summary)]
     captions = webvtt.read(subtitle)
-    coptions = webvtt.read(subtitle)
-    cuttimes = []
-    for sent in sents[-5:]:
-        beg = ' '.join(sent[:3])
-        last = ' '.join(sent[-4:-1])
-        for i, caption in enumerate(captions):
-            captext = caption.text.replace("\n", " ")
-            captext = filter_captext(captext)
-            if beg in captext:
-                start = caption.start
-                break
-
-        for coption in coptions:
-            captext = coption.text.replace("\n", " ")
-            captext = filter_captext(captext)
-            if last in captext:
-                s, e = time_in_s([start, coption.end])
-                if e-s>0:
-                    end = coption.end
-                    break
-        cuttimes.append([start, end])
-        break
-
-    return cuttimes
 
 
 def tolerance_manager(cuttimes, tolerance):
@@ -52,12 +57,12 @@ def tolerance_manager(cuttimes, tolerance):
         if cstart-pend<=tolerance:
             flag=True
             newcuttimes.append([pstart, cend])
-            pstart, pend=0
+            pstart, pend=0,0
         else:
             newcuttimes.append([pstart, pend])
             pstart, pend = cstart, cend
     if pstart and pend:
-        newcuttimes.append()
+        newcuttimes.append([pstart, pend])
 
     if flag==True:
         return tolerance_manager(newcuttimes, tolerance)
@@ -80,11 +85,6 @@ def time_in_s(clip):
         s+=float(start[i])*pow(60, 2-i)
         e+=float(end[i])*pow(60, 2-i)
     return s, e
-
-    return cuttimes
-    # for caption in webvtt.read(subtitle)[:100]:
-    #     print(caption.start)
-    #     print(caption.text)
 
 def filter_captext(captext):
     apos = captext.find("'")
