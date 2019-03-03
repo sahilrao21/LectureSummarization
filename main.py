56a3b9d0383fac1083ae2a0a42b895698125a064
eashman123 <eashaank@gmail.com>
2019-03-03 03:21:09 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, request
-import dl_youtube
-import transcript_of_captions
+from flask import Flask, render_template, request, redirect
+import dl_youtube, os
+from puncuator import punctuate_transcript
 
 app = Flask(__name__)
+
 @app.route("/")
 def search_home():
-   return render_template("index.html")
+    return render_template("index.html")
 
 @app.route("/transcript", methods=['POST'])
 def transcript():
@@ -16,10 +17,10 @@ def transcript():
     # audio_yn = request.form['download_audio']
     assert the_link is not '', "Please Input a Link"
     assert percentage_size>=0.33 and percentage_size <= 1, "Please Input a Valid Proportion"
-    vtt = dl_youtube.video_download(the_link, False)
-    the_transcript = transcript_of_captions.generate_transcript(vtt)
-
-    return render_template("transcript.html", transc=the_transcript)
+    vtt = dl_youtube.video_download(the_link, False)[1]
+    the_transcript = punctuate_transcript(vtt)
+    # return render_template("transcript.html", transc=the_transcript)
+    return redirect(os.getcwd()+vtt)
 
 
 
