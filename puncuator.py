37bbe2d26d171c22c05d8e036aae89a6be3d1973
eashman123 <eashaank@gmail.com>
2019-03-03 00:17:19 -0800

    Return Behavior Changed
    Returns name of text file

--- puncuator.py
+++ puncuator.py
@@ -10,6 +10,7 @@ Subtitle Filename
 
 Output:
 Trascript as a Text File including punctuation, in the format video_id.txt
+Returns name of the text file.
 
 ML Model from the Institute of Cybernetics of Tut
 """
@@ -20,10 +21,11 @@ def punctuate_transcript(subtitle):
     t = generate_transcript(subtitle)
     if "auto" not in subtitle:
         open(name+".txt", "w").write(t)
-        return t
+        return name+".txt"
     else:
         r = post('http://bark.phon.ioc.ee/punctuator', data={'text': t})
         open(name[:-5]+".txt", "w").write(r.text)
+        return name[:-5]+".txt"
 
 if __name__=="__main__":
     subtitle = "fQgupEKPsbc.en.vtt"
