01a3b3ce456b7bd27527a1d15d9ec8832c8b0632
eashman123 <eashaank@gmail.com>
2019-03-03 00:08:12 -0800

    Added Documentation and Attributions

--- puncuator.py
+++ puncuator.py
@@ -1,17 +1,30 @@
 from transcript_of_captions import generate_transcript
 from requests import post
 
+
+"""
+Ensures that subtitle files are punctuated
+
+Input:
+Subtitle Filename
+
+Output:
+Trascript as a Text File including punctuation, in the format video_id.txt
+
+ML Model from the Institute of Cybernetics of Tut
+"""
+
 def punctuate_transcript(subtitle):
     i = subtitle.find(".en.vtt")
     name = subtitle[:i]
     t = generate_transcript(subtitle)
     if "auto" not in subtitle:
-        open(name, "w").write(t)
+        open(name+".txt", "w").write(t)
         return t
     else:
         r = post('http://bark.phon.ioc.ee/punctuator', data={'text': t})
         open(name[:-5]+".txt", "w").write(r.text)
 
 if __name__=="__main__":
-    subtitle = "zbf3ILq9IJs_auto.en.vtt"
+    subtitle = "fQgupEKPsbc.en.vtt"
     punctuate_transcript(subtitle)
