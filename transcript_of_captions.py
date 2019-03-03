d2ce7a5df57049569bc0781dd4accdb1522b568f
Sahil Rao <sahil.rao@berkeley.edu>
2019-03-02 18:44:58 -0800

    cleaning up file and adding function docs

--- transcript_of_captions.py
+++ transcript_of_captions.py
@@ -1,6 +1,5 @@
 import webvtt
 
-#
 def generate_transcript(file):
     """Generates the transcript of a given .vtt file."""
     if len(file) < 4 or file[-4:] != ".vtt":
@@ -24,43 +23,17 @@ def generate_transcript(file):
 
     return transcript
 
-
-#def ignore_descriptions(text):
-#    """Avoids speaker names or other descriptions provided by captions."""
-#    print("HERE")
-#    if text == "":
-#        return text
-
-#    symbols_to_avoid = ["[", "]", "{", "}"]
-#    text_without_descrip = ""
-#    for chars in text:
-#        print(chars)
-#        if (not chars[0].isalpha() and chars[0] not in symbols_to_avoid) or \
-#                (chars[0].isalpha() and chars[0].islower()):
-#            text_without_descrip += chars
-
-#    return text_without_descrip
-
 def ignore_descriptions(text):
+    """Taking out information in brackets that are auto-generated."""
     start_index = text.find("[")
     end_index = text.find("]")
-    curr_text = ""
-    #count = 0
 
-    if start_index == -1 or end_index == -1: #and count == 0:
+    if start_index == -1 or end_index == -1:
         return text
-
-    #while start_index != -1:
-    #+= text[0: start_index] + text[end_index + 1:]
-
-    #start_index = curr_text.find("[")
-    #end_index = curr_text.find("]")
-    #count += 1
-
-    #return curr_text
     return text[: start_index - 1] + ignore_descriptions(text[end_index + 1:])
 
 def ignore_names(text):
+    """Ignore captions that give the name of the speaker."""
     colon_index = text.find(":")
     print("COL_INDEX", colon_index)
     if colon_index == -1 or len(text) <= 1:
@@ -82,22 +55,3 @@ def ignore_names(text):
     name = reverse_name[::-1]
 
     return text[:text.find(name)] + ignore_names(text[colon_index + 1:])
-
-
-
-
-
-
-#print(ignore_descriptions("hello there [my friend, ok], yes sir."))
-#print(ignore_names("DAN GARCIA: yes "))
-#print(ignore_names("DAN GARCIA: yes Sahil haha. SAHIL RAO: oh no. jamES CARL: ye"))
-#ci = "DAN GARCIA: yes Sahil haha. SAHIL RAO: oh no. jamES CARL: ye"["DAN GARCIA: yes Sahil haha. SAHIL RAO: oh no. jamES CARL: ye".find(":") + 2:]
-ci = "yes Sahil haha. SAHIL RAO: oh no. jamES CARL: ye"
-#print(ci)
-#ci2 = ci.find(":")
-#print(ignore_names(ci))
-
-print(ignore_names(ci))
-#print(generate_transcript("COMPSCI 170 - 2019-02-19-f7fri6FsvvQ.en.vtt"))
-#print([1,2,3][0:-1])
-#print(list(map(lambda x: 1, ['s','a','h','u','k'])))
