ae6de5f877dc2f3ce10675ce0a95a4dc36759472
eashman123 <eashaank@gmail.com>
2019-03-03 02:07:15 -0800

    Added form for lecture proportion

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ def search_home():
 @app.route("/transcript", methods=['POST'])
 def transcript():
     the_link = request.form['link']
+    percentage_size = float(request.form['proportion'])
     # the_cookies = request.form['cookies']
     # video_yn = request.form['download_video']
     # audio_yn = request.form['download_audio']
-    vtt = dl_youtube.video_download(the_link, 22)
+    assert the_link is not '', "Please Input a Link"
+    assert percentage_size>=0.33 and percentage_size <= 1, "Please Input a Valid Proportion"
+    vtt = dl_youtube.video_download(the_link, False)
     the_transcript = transcript_of_captions.generate_transcript(vtt)
 
     return render_template("transcript.html", transc=the_transcript)
